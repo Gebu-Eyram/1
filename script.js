33905fdb61dbe668370fd2038bab922ad802fe34
Heny <gebueyram18@gmail.com>
2023-12-18 04:05:37 +0000

    Added more functionality to header and changed nav ul

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const menu = document.getElementById('sidemenu')
-
+const header = document.querySelector(".headersection");
 const body = document.querySelector(".body");
 bar = document.getElementById("bars")
 function openmenu(){
 
 menu.style.right = "0"
-bar.style.opacity = "0"
+
 }
 
 function closemenu(){
-
+    
     menu.style.right = "-200px"
-    bar.style.opacity = "1"   
+    bar.style.opacity = "1"  
+    header.style.top = "-80px" 
 }
 
 
 
-const header = document.querySelector(".headersection");
+
 let lastScrollTop = 0;
 window.addEventListener("scroll",function(){
     let scrollTop = window.pageYOffset || this.document.documentElement.scrollTop;

7efe0ff8467a41780d34255c3e1ca1a2b378b916
Heny <gebueyram18@gmail.com>
2023-12-18 03:45:07 +0000

    Added scroll features

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const menu = document.getElementById('sidemenu')
 
+const body = document.querySelector(".body");
+bar = document.getElementById("bars")
 function openmenu(){
 
 menu.style.right = "0"
+bar.style.opacity = "0"
 }
 
 function closemenu(){
 
     menu.style.right = "-200px"
+    bar.style.opacity = "1"   
+}
+
+
+
+const header = document.querySelector(".headersection");
+let lastScrollTop = 0;
+window.addEventListener("scroll",function(){
+    let scrollTop = window.pageYOffset || this.document.documentElement.scrollTop;
+    if(scrollTop > lastScrollTop){
+        header.style.top = "-80px"
     }
+    else{
+        header.style.top = "0"
+    }
+    lastScrollTop =scrollTop
+})
+
 
 menu.addEventListener("click",closemenu)
+body.addEventListener("click",closemenu)
